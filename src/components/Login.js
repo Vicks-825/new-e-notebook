0095ce7002e0bf9c1000a374b72a5f25505f18bf
Vicks <vk82562425@gmail.com>
2023-10-24 13:37:22 +0530

    updated js package, signup and login

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,12 +2,11 @@ import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
-    const host = 'https://my-online-e-notebook.herokuapp.com';
     const [credential, setCredential] = useState({email: "", password: ""});
     let navigate = useNavigate();
     const handleSubmit = async(e)=>{
         e.preventDefault();
-        const response = await fetch(`${host}/api/auth/login`, {
+        const response = await fetch(`/api/auth/login`, {
             method: 'POST', // *GET, POST, PUT, DELETE, etc.
             headers: {
               'Content-Type': 'application/json'
